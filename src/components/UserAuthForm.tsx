fc1b6115e79944863098d5063ca9c7b77f44bace
Kevin Simon <keviniansimon@gmail.com>
2023-06-19 11:01:05 +0200

    build: handle user auth error

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -1,17 +1,43 @@
+'use client';
+
+import { useToast } from '@/hooks/use-toast';
 import { cn } from '@/lib/utils';
-import { FC } from 'react';
+import { signIn } from 'next-auth/react';
+import { FC, useState } from 'react';
+import { Icons } from './Icons';
 import { Button } from './ui/Button';
 
-interface UserAuthFormProps
-  extends React.HTMLAttributes<HTMLDivElement> {}
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
-const UserAuthForm: FC<UserAuthFormProps> = ({
-  className,
-  ...props
-}) => {
+const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { toast } = useToast();
+
+  const loginWithGoogle = async () => {
+    setIsLoading(true);
+
+    try {
+      await signIn('google');
+    } catch (error) {
+      toast({
+        title: 'There was a problem',
+        description: 'There was an error logging in with Google',
+        variant: 'destructive',
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className={cn('flex justify-center', className)} {...props}>
-      <Button size="sm" className="w-full">
+      <Button
+        onClick={loginWithGoogle}
+        isLoading={isLoading}
+        size="sm"
+        className="w-full"
+      >
+        {isLoading ? null : <Icons.google className="h-4 w-4 mr-2" />}
         Google
       </Button>
     </div>
