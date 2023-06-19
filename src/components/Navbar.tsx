ff9c9538040d919a67827e1378fafaf3cc9adb84
Kevin Simon <keviniansimon@gmail.com>
2023-06-19 12:00:03 +0200

    fix: fixed navbar loading

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,8 @@
-import { getAuthSession } from '@/lib/auth';
 import Link from 'next/link';
 import { Icons } from './Icons';
 import { buttonVariants } from './ui/Button';
 
-const Navbar = async () => {
-  const session = await getAuthSession();
-
+const Navbar = () => {
   return (
     <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
